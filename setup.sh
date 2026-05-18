cf855a4da51bfc259d2de9ebf799f94166f44864
Jon Binney <jon.binney@gmail.com>
2026-05-18 13:34:34 -0400

    Source virtual env

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+# Source this script from the root of deep_rabbit_hole
 
+. .venv/bin/activate
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 export PATH=$SCRIPT_DIR/bin:$PATH
